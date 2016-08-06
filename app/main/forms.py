fc7668de55261f519cc5238667208ea51240fae5
Rouz Azari <rouz.azari@gmail.com>
2016-08-06 11:00:58 -0700

    added dynamic angularjs widget class.

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,29 +5,34 @@ from wtforms.validators import DataRequired
 from wtforms.widgets import TextInput
 
 
-class AngularJSTextInput(TextInput):
+def get_angularjs_class(base_class):
     """
-    Create a widget to allow "ng-" attributes for wtforms.
+    Returns a dynamic class widget to allow "ng-" attributes for wtforms.
+    Parameter base_class is a wtforms.widget class.
     Template requires argument of ng_*='value' to set the attribute.
 
-    Source:
+    Adapted version of solution from:
     http://stackoverflow.com/questions/20440056/custom-attributes-for-flask-wtforms
     """
-    def __call__(self, field, **kwargs):
-        for key in list(kwargs):
-            if key.startswith('ng_'):
-                kwargs['ng-' + key[3:]] = kwargs.pop(key)
-        return super(AngularJSTextInput, self).__call__(field, **kwargs)
+    class AngularJSClass(base_class):
+
+        def __call__(self, field, **kwargs):
+            for key in list(kwargs):
+                if key.startswith('ng_'):
+                    kwargs['ng-' + key[3:]] = kwargs.pop(key)
+            return super(AngularJSClass, self).__call__(field, **kwargs)
+
+    return AngularJSClass
 
 
 class DataForm(Form):
     """
-    Input field to test {widget=AngularJSTextInput}.
+    Input field for test {widget=AngularJSClass(TextInput}.
     """
     name = StringField('What is your name?',
                        validators=[DataRequired()],
-                       widget=AngularJSTextInput())
+                       widget=get_angularjs_class(TextInput)())
     location = StringField('What is your location?',
                            validators=[DataRequired()],
-                           widget=AngularJSTextInput())
+                           widget=get_angularjs_class(TextInput)())
     submit = SubmitField('Submit')

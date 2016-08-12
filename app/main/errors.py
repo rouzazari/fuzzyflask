2b2e53aec8680e8e8b909b280c0027c1e719c4e3
Rouz Azari <rouz.azari@gmail.com>
2016-08-12 14:10:36 -0700

    Fixed bug with HTTP error handlers (not rendering templates due to wrong decorator).

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,16 +2,16 @@ from flask import render_template
 from . import main
 
 
-@main.errorhandler(403)
-def page_not_found(e):
+@main.app_errorhandler(403)
+def page_forbidden(e):
     return render_template('403.html'), 403
 
 
-@main.errorhandler(404)
+@main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@main.errorhandler(500)
+@main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
